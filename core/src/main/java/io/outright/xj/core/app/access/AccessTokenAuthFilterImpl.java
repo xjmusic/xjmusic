28e5142a020ffde24883c43be58c21edfd52b029
Charney Kaye <charney@outright.io>
2017-02-01 18:24:06 -0500

    [core] FIX: Admin always passes route role filter

--- core/src/main/java/io/outright/xj/core/app/access/AccessTokenAuthFilterImpl.java
+++ core/src/main/java/io/outright/xj/core/app/access/AccessTokenAuthFilterImpl.java
@@ -83,7 +83,7 @@ public class AccessTokenAuthFilterImpl implements AccessTokenAuthFilter {
       return denied(context, "invalid access_token");
     }
 
-    if (!accessControl.matchRoles(aRolesAllowed.value())) {
+    if (!accessControl.isAdmin() && !accessControl.matchRoles(aRolesAllowed.value())) {
       return denied(context, "user has no accessible role");
     }
 
