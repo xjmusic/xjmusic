9d60311d082a5e9012daa1ee0fb278f7422fa00e
Charney Kaye <charney@outright.io>
2017-02-21 20:52:17 -0500

    [#123] Time UTC with seconds in navbar
    Including:
      * [ui] Ember Moment.js https://github.com/stefanpenner/ember-moment https://momentjs.com/

--- ui/app/controllers/application.js
+++ ui/app/controllers/application.js
@@ -1,11 +1,31 @@
 // Copyright (c) 2017, Outright Mental Inc. (http://outright.io) All Rights Reserved.
-import Ember from 'ember';
+import Ember from "ember";
+// import momentComputed from 'ember-moment/computeds/moment';
+// import format from 'ember-moment/computeds/format';
+// import locale from 'ember-moment/computeds/locale';
+import moment from "moment";
 
 export default Ember.Controller.extend({
   auth: Ember.inject.service(),
+
   needs: ['application'],
 
-  actions: {
+  timeNowUTC: '',
 
-  }
+  init() {
+    let ctrl = this;
+    // moment().setTimezone('UTC');
+    setInterval(function () {
+      let now = new Date();
+      let nowUTC = moment(
+        new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds())
+      ).format("YYYY-MM-DD HH:mm:ss");
+      if (nowUTC !== ctrl.get('nowUTC')) {
+        ctrl.set('timeNowUTC', nowUTC);
+        document.getElementById('current-time-utc').innerHTML = nowUTC;
+      }
+    }, 250);
+  },
+
+  actions: {}
 });
