fb7287c798bbc0401fb8cbb3bec3acc2947f7183
Alice Gamarnik <agamarnik1@pride.hofstra.edu>
2017-07-21 14:47:30 -0400

    [#243] In the Chain-Links index view, ought to be able to click the chosen idea for any link, and navigate to that idea.
    Including:
      * [ui] Now able to click an idea from any link, and navigate to that idea.
      * [worker] cleanup unused code; add comments

--- worker/src/main/java/io/xj/worker/Main.java
+++ worker/src/main/java/io/xj/worker/Main.java
@@ -43,77 +43,15 @@ public class Main {
     try {
       MigrationService.validate(sqlDatabaseProvider);
     } catch (ConfigException e) {
-      log.error("Migrations failed! App will not start.", e);
+      log.error("Migration validation failed! Worker App will not start.", e);
       System.exit(1);
     }
 
     // Server App
     app.configureServer("io.xj.worker");
 
-    // Set App Job Factory
+    // Set App Job Factory - Note that this is the only line that makes this app a worker!
     app.setJobFactory(jobFactory);
-
-/*
-    // Start a worker to run jobs from the queue
-    Thread workerThread = new Thread(worker);
-    workerThread.start();
-
-    // Try to join that thread
-    try {
-      workerThread.join();
-    } catch (Exception e) {
-      log.error("Joining worker thread", e);
-    }
-*/
-
-/*
-
-    // Pilot-type Workload
-    ChainGangFactory pilotChainGangFactory = Guice.createInjector(new CoreModule(),
-      new LinkPilotWorkFactoryModule()).getInstance(ChainGangFactory.class);
-    app.registerGangWorkload(
-      "Create New Links",
-      pilotChainGangFactory.createLeader(Config.workBufferSeconds(), Config.workBatchSize()),
-      pilotChainGangFactory.createFollower(LinkState.Planned)
-    );
-
-    // Link-type Workload
-    ChainGangOperation craftOperation = Guice.createInjector(new CoreModule(),
-      new CraftworkerModule()).getInstance(ChainGangOperation.class);
-    ChainGangFactory craftFactory = Guice.createInjector(new CoreModule(),
-      new LinkWorkFactoryModule()).getInstance(ChainGangFactory.class);
-    app.registerGangWorkload(
-      "Craft Links",
-      craftFactory.createLeader(LinkState.Planned, Config.workBufferSeconds(), Config.workBatchSize()),
-      craftFactory.createFollower(LinkState.Crafting, LinkState.Crafted, craftOperation)
-    );
-
-    // Link-type Workload
-    ChainGangOperation dubOperation = Guice.createInjector(new CoreModule(), new MixerModule(),
-      new DubworkerModule()).getInstance(ChainGangOperation.class);
-    ChainGangFactory dubFactory = Guice.createInjector(new CoreModule(), new MixerModule(),
-      new LinkWorkFactoryModule()).getInstance(ChainGangFactory.class);
-    app.registerGangWorkload(
-      "Dub Links",
-      dubFactory.createLeader(LinkState.Crafted, Config.workBufferSeconds(), Config.workBatchSize()),
-      dubFactory.createFollower(LinkState.Dubbing, LinkState.Dubbed, dubOperation)
-    );
-
-    // Worker (Guice) factory
-    EraseFactory eraseFactory = Guice.createInjector(new CoreModule(),
-      new EraseworkerModule()).getInstance(EraseFactory.class);
-
-    // Chain-Erase Workload
-    app.registerSimpleWorkload(
-      "Erase Chains",
-      eraseFactory.chainEraseWorker(Config.workBatchSize()));
-
-    // Audio-Erase Workload
-    app.registerSimpleWorkload(
-      "Erase Audio",
-      eraseFactory.audioEraseWorker(Config.workBatchSize()));
-
-*/
 
     // Shutdown Hook
     Runtime.getRuntime().addShutdownHook(new Thread(Main::shutdown));
