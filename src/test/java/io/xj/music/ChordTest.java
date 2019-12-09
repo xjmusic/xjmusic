975184fb2a6aac7fbe7faf7402ae8167dac6f24a
Charney <charneykaye@gmail.com>
2019-12-09 13:55:51 -0800

    [#170196030] One source tree   - Instead of having separate folders for separate dependent Maven projects, merge everything into one project `platform` with all xj platform code under one Java source tree.

---
+++ src/test/java/io/xj/music/ChordTest.java
@@ -0,0 +1,172 @@
+// Copyright (c) 2020, XJ Music Inc. (https://xj.io) All Rights Reserved.
+package io.xj.music;
+
+import com.google.common.collect.Maps;
+
+import io.xj.music.schema.IntervalPitchGroup;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import java.math.BigInteger;
+import java.util.Map;
+
+import static io.xj.music.Interval.I3;
+import static io.xj.music.Interval.I6;
+import static io.xj.music.Interval.I7;
+import static io.xj.music.Interval.I9;
+import static io.xj.music.PitchClass.A;
+import static io.xj.music.PitchClass.As;
+import static io.xj.music.PitchClass.C;
+import static io.xj.music.PitchClass.Cs;
+import static io.xj.music.PitchClass.D;
+import static io.xj.music.PitchClass.Ds;
+import static io.xj.music.PitchClass.F;
+import static io.xj.music.PitchClass.Fs;
+import static io.xj.music.PitchClass.None;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class ChordTest {
+  private static final String EXPECTED_CHORDS_YAML = "/music/expect_chord.yaml";
+  private static final String KEY_CHORDS = "chords";
+  private static final String KEY_ROOT_PITCH_CLASS = "root";
+  private static final Object KEY_PITCHES = "pitches";
+
+  @Test
+  public void TestChordExpectations() throws Exception {
+    Yaml yaml = new Yaml();
+
+    Map<?, ?> wrapper = (Map<?, ?>) yaml.load(getClass().getResourceAsStream(EXPECTED_CHORDS_YAML));
+    assertNotNull(wrapper);
+
+    Map<?, ?> chords = (Map<?, ?>) wrapper.get(KEY_CHORDS);
+    assertNotNull(chords);
+
+    chords.keySet().forEach((chordName) -> {
+      Map<?, ?> chord = (Map<?, ?>) chords.get(chordName);
+      PitchClass expectRootPitchClass = PitchClass.of(String.valueOf(chord.get(KEY_ROOT_PITCH_CLASS)));
+      assertNotNull(expectRootPitchClass);
+
+      Map<?, ?> rawPitches = (Map<?, ?>) chord.get(KEY_PITCHES);
+      assertNotNull(rawPitches);
+
+      Map<Interval, PitchClass> expectPitches = Maps.newHashMap();
+      rawPitches.forEach((rawInterval, rawPitchClass) ->
+        expectPitches.put(
+          Interval.valueOf(Integer.valueOf(String.valueOf(rawInterval))),
+          PitchClass.of(String.valueOf(rawPitchClass))));
+
+      assertChordExpectations(expectRootPitchClass, expectPitches, Chord.of(String.valueOf(chordName)));
+    });
+  }
+
+  private void assertChordExpectations(PitchClass expectRootPitchClass, Map<Interval, PitchClass> expectPitchClasses, Chord chord) {
+    System.out.println(
+      "Expect pitch classes " + IntervalPitchGroup.detailsOf(expectPitchClasses, chord.getAdjSymbol()) + " for " +
+        "Chord " + chord.details());
+    Map<Interval, PitchClass> pitchClasses = chord.getPitchClasses();
+    assertEquals(expectRootPitchClass, chord.getRootPitchClass());
+    assertEquals("same number of pitch classes", expectPitchClasses.size(), pitchClasses.size());
+    expectPitchClasses.forEach((expectInterval, expectPitchClass) ->
+      assertEquals(String.format("same pitch class at interval %s", expectInterval.getValue()), expectPitchClass, pitchClasses.get(expectInterval)));
+  }
+
+  @Test
+  public void TestOf_Invalid() {
+    Chord chord = Chord.of("P-funk");
+    assertEquals(None, chord.getRootPitchClass());
+  }
+
+  @Test
+  public void TestChordOf() {
+    Chord chord = Chord.of("Cm nondominant -5 +6 +7 +9");
+    Map<Interval, PitchClass> pitchClasses = chord.getPitchClasses();
+    assertEquals(4, pitchClasses.size());
+    assertEquals(Ds, pitchClasses.get(I3));
+    assertEquals(A, pitchClasses.get(I6));
+    assertEquals(As, pitchClasses.get(I7));
+    assertEquals(D, pitchClasses.get(I9));
+  }
+
+  @Test
+  public void TestTranspose() {
+    Chord chord = Chord.of("Cm nondominant -5 +6 +7 +9").transpose(3);
+    Map<Interval, PitchClass> pitchClasses = chord.getPitchClasses();
+    assertEquals(4, pitchClasses.size());
+    assertEquals(Fs, pitchClasses.get(I3));
+    assertEquals(C, pitchClasses.get(I6));
+    assertEquals(Cs, pitchClasses.get(I7));
+    assertEquals(F, pitchClasses.get(I9));
+  }
+
+  @Test
+  public void TestTranspose_DescriptionChange() {
+    Chord chord = Chord.of("Cm nondominant -5 +6 +7 +9");
+
+    assertEquals("C m nondominant -5 +6 +7 +9", chord.getFullDescription());
+    assertEquals("Eb m nondominant -5 +6 +7 +9", chord.transpose(3).getFullDescription());
+  }
+
+  @Test
+  public void TestTranspose_officialDescription() {
+    Chord chord = Chord.of("Cm nondominant -5 +6 +7 +9");
+
+    assertEquals("C Basic NonDominant Minor Triad Omit Fifth Add Sixth Add Seventh Add Ninth", chord.officialDescription());
+  }
+
+  @Test
+  public void getForms_setForms() {
+    Chord source = Chord.of("Cm nondominant -5 +6 +7 +9");
+    Chord result = new Chord();
+    result.setForms(source.getForms());
+
+    assertEquals(7, result.getForms().size());
+  }
+
+  @Test
+  public void formString() {
+    Chord chord = Chord.of("Cm nondominant -5 +6 +7 +9");
+
+    assertEquals("Basic NonDominant Minor Triad Omit Fifth Add Sixth Add Seventh Add Ninth", chord.formString());
+  }
+
+  /**
+   [#154985948] Architect wants to determine tonal similarity (% of shared pitch classes) between two Chords, in order to perform fuzzy matching operations.
+   */
+  @Test
+  public void similarity() {
+    assertEquals(1.0000,
+      Chord.of("Cm nondominant -5 +6 +7 +9").similarity(
+        Chord.of("Cm nondominant -5 +6 +7 +9")), 0.001);
+
+    assertEquals(0.2499,
+      Chord.of("Cm nondominant -5 +6 +7 +9").similarity(
+        Chord.of("Gm nondominant -5 +6 +7 +9")), 0.001);
+
+    assertEquals(0.3125,
+      Chord.of("Cm nondominant -5 +6 +7 +9").similarity(
+        Chord.of("D7 minor")), 0.001);
+
+    assertEquals(0.1042,
+      Chord.of("Cm nondominant -5 +6 +7 +9").similarity(
+        Chord.of("C major")), 0.001);
+
+    assertEquals(0.6250,
+      Chord.of("C minor 7").similarity(
+        Chord.of("C major 7")), 0.001);
+
+    assertEquals(0.7792,
+      Chord.of("C minor 7").similarity(
+        Chord.of("C major m7")), 0.001);
+
+  }
+
+  @Test
+  public void isNull() {
+    assertFalse(Chord.of("C#m7").isNoChord());
+    assertTrue(Chord.of("NC").isNoChord());
+  }
+
+}
---
+++ src/test/java/io/xj/music/KeyTest.java
@@ -0,0 +1,115 @@
+// Copyright (c) 2020, XJ Music Inc. (https://xj.io) All Rights Reserved.
+package io.xj.music;
+
+import io.xj.music.schema.KeyMode;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class KeyTest {
+
+  private static final String EXPECTED_KEYS_YAML = "/music/expect_key.yaml";
+  private static final String KEY_KEYS = "keys";
+  private static final String KEY_ROOT_PITCH_CLASS = "root";
+  private static final Object KEY_MODE = "mode";
+
+  @Test
+  public void KeyExpectationsTest() throws Exception {
+    Yaml yaml = new Yaml();
+
+    Map<?, ?> wrapper = (Map<?, ?>) yaml.load(getClass().getResourceAsStream(EXPECTED_KEYS_YAML));
+    assertNotNull(wrapper);
+
+    Map<?, ?> keys = (Map<?, ?>) wrapper.get(KEY_KEYS);
+    assertNotNull(keys);
+
+    keys.keySet().forEach((keyName) -> {
+      Map<?, ?> key = (Map<?, ?>) keys.get(keyName);
+      PitchClass expectRootPitchClass = PitchClass.of(String.valueOf(key.get(KEY_ROOT_PITCH_CLASS)));
+      assertNotNull(expectRootPitchClass);
+
+      KeyMode expectMode = KeyMode.of(String.valueOf(key.get(KEY_MODE)));
+      assertNotNull(expectMode);
+
+      assertKeyExpectations(expectRootPitchClass, expectMode, Key.of(String.valueOf(keyName)));
+    });
+  }
+
+  private void assertKeyExpectations(PitchClass expectRootPitchClass, KeyMode expectMode, Key key) {
+    System.out.println(
+      "Expect pitch classes " + expectMode + " for " +
+        "Key '" + key.getName() + "'");
+    Assert.assertEquals(expectRootPitchClass, key.getRootPitchClass());
+    Assert.assertEquals(expectMode, key.getMode());
+  }
+
+  @Test
+  public void isMatchingMode() throws Exception {
+    assertTrue(Key.isSameMode("C minor", "G minor"));
+  }
+
+  @Test
+  public void delta() throws Exception {
+    assertEquals(Integer.valueOf(2), Key.delta("C", "D", 0));
+    assertEquals(Integer.valueOf(-5), Key.delta("C", "G", 0));
+    assertEquals(Integer.valueOf(-3), Key.delta("C", "G", 2));
+  }
+
+  @Test
+  public void OfInvalidTest() {
+    Key key = Key.of("P-funk");
+    Assert.assertEquals(PitchClass.None, key.getRootPitchClass());
+  }
+
+
+  @Test
+  public void PitchClassDiffTest() {
+    Assert.assertEquals(5, Key.of("C#").delta(Key.of("F#")));
+    Assert.assertEquals(-5, Key.of("F#").delta(Key.of("C#")));
+    Assert.assertEquals(2, Key.of("Gb").delta(Key.of("Ab")));
+    Assert.assertEquals(-3, Key.of("C").delta(Key.of("A")));
+    Assert.assertEquals(4, Key.of("D").delta(Key.of("F#")));
+    Assert.assertEquals(-6, Key.of("F").delta(Key.of("B")));
+  }
+
+  @Test
+  public void RelativeMajorTest() {
+    Key key = Key.of("A minor").relativeMajor();
+    Assert.assertEquals(PitchClass.C, key.getRootPitchClass());
+    Assert.assertEquals(KeyMode.Major, key.getMode());
+  }
+
+  @Test
+  public void RelativeMinorTest() {
+    Key key = Key.of("C major").relativeMinor();
+    Assert.assertEquals(PitchClass.A, key.getRootPitchClass());
+    Assert.assertEquals(KeyMode.Minor, key.getMode());
+  }
+
+  @Test
+  public void ModeStringTest() {
+    Assert.assertEquals("Major", KeyMode.Major.toString());
+    Assert.assertEquals("Minor", KeyMode.Minor.toString());
+    assertEquals("None", KeyMode.None.toString());
+  }
+
+  @Test
+  public void ModeOfTest() {
+    Assert.assertEquals(KeyMode.Major, KeyMode.of("Major"));
+    Assert.assertEquals(KeyMode.Major, KeyMode.of("M"));
+    Assert.assertEquals(KeyMode.Major, KeyMode.of("major"));
+
+    Assert.assertEquals(KeyMode.Minor, KeyMode.of("minor"));
+    Assert.assertEquals(KeyMode.Minor, KeyMode.of("min"));
+    Assert.assertEquals(KeyMode.Minor, KeyMode.of("m"));
+
+    Assert.assertEquals(KeyMode.Major, KeyMode.of("joe"));
+  }
+}
