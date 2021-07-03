db6355269352b10a7a8f55b402ed2dbca973b52a
Charney <charneykaye@gmail.com>
2021-07-03 12:13:02 -0700

    Log message cleanup

--- src/main/java/io/xj/nexus/work/NexusWorkImpl.java
+++ src/main/java/io/xj/nexus/work/NexusWorkImpl.java
@@ -367,8 +367,7 @@ public class NexusWorkImpl implements NexusWork {
         didFailWhile("finishing work", e, segment.getId(), ChainDAO.getIdentifier(chain), chain.getType().toString());
       }
 
-      LOG.info("{} Chain[{}] offset={} Segment[{}] fabricated ahead +{}s to {}s",
-        chain.getType(),
+      LOG.info("Chain[{}] offset={} Segment[{}] fabricated ahead +{}s to {}s",
         ChainDAO.getIdentifier(chain),
         segment.getOffset(),
         segmentDAO.getIdentifier(segment),
