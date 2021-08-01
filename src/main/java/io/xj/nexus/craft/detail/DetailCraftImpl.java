a758e7edc9f91540656c0c43bce1b8bf22d23d36
Charney Kaye <charneykaye@gmail.com>
2021-08-01 16:30:38 -0700

    No arcs alpha-- nonsense

--- src/main/java/io/xj/nexus/craft/detail/DetailCraftImpl.java
+++ src/main/java/io/xj/nexus/craft/detail/DetailCraftImpl.java
@@ -3,7 +3,6 @@ package io.xj.nexus.craft.detail;
 
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
-import io.xj.Chain;
 import io.xj.Instrument;
 import io.xj.Program;
 import io.xj.ProgramVoice;
@@ -96,26 +95,13 @@ public class DetailCraftImpl extends ArrangementCraftImpl implements DetailCraft
             .setSegmentId(fabricator.getSegment().getId())
             .build());
 
-          // Optionally, use the inertial choice that corresponds to this primary one, instead.
+          // FUTURE: Optionally, use the inertial choice that corresponds to this primary one, instead.
           //          var inertialChoice = computeInertialChoice(primaryChoice);
-
-          // TEMPORARY
-          //   Arcs Alpha: % chance of playing each layer
-          //   https://www.pivotaltracker.com/story/show/179081564
-          if (Chain.Type.Production.equals(fabricator.getChain().getType()) &&
-            TremendouslyRandom.beatOddsAgainstOne(switch (voicingType) {
-              case UNRECOGNIZED, Percussive, Bass -> 0;
-              case Pad -> 5 + fabricator.getSegment().getDelta();
-              case Sticky -> 1 + fabricator.getSegment().getDelta();
-              case Stripe -> 2 + fabricator.getSegment().getDelta();
-              case Stab -> 3 + fabricator.getSegment().getDelta();
-            })) return;
-
           // If there's an inertial choice, use it
-//          if (inertialChoice.isPresent())
-//            this.craftArrangements(fabricator.add(inertialChoice.get()));
-//          else
-            this.craftArrangements(primaryChoice);
+          //          if (inertialChoice.isPresent())
+          //            this.craftArrangements(fabricator.add(inertialChoice.get()));
+          //          else
+          this.craftArrangements(primaryChoice);
         }
       }
     }
