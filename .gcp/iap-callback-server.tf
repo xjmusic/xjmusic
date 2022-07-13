e6a0b155323e18dee643af96595f5b080958a3a5
Charney <charneykaye@gmail.com>
2022-07-12 23:31:53 -0700

    Scaffold feedback app on google app engine

---
+++ .gcp/iap-callback-server.tf
@@ -0,0 +1,9 @@
+# Copyright (c) XJ Music Inc. (https://xj.io) All Rights Reserved.
+
+# Server to receive callbacks for in-app purchase notifications
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/app_engine_application
+resource "google_app_engine_application" "app" {
+  project     = local.gcp-project-id
+  location_id = local.gcp-region
+}
