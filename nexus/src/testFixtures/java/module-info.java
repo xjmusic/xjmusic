4bef897f84729d8ce312d56fd73e18e1999fcf5c
Charney <charneykaye@gmail.com>
2023-08-08 17:37:08 -0700

    cleanup

--- nexus/src/testFixtures/java/module-info.java
+++ nexus/src/testFixtures/java/module-info.java
@@ -1,7 +1,8 @@
 module workstation.nexus.testFixtures {
-  requires java.sql;
   requires jakarta.annotation;
+  requires java.sql;
   requires org.slf4j;
+  requires spring.boot.starter;
   //
   requires workstation.lib.main;
   requires workstation.nexus.main;
