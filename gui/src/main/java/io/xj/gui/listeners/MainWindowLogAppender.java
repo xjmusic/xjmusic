580cc27dbd03f7fc85e455328467d933637571c2
Charney <charneykaye@gmail.com>
2023-08-11 20:25:33 -0700

    No main log appender yet

--- gui/src/main/java/io/xj/gui/listeners/MainWindowLogAppender.java
+++
@@ -1,8 +0,0 @@
-package io.xj.gui.listeners;
-
-public class MainWindowLogAppender {
-  public static void append(String message) {
-    var butts=123;// todo
-    // todo textArea.appendText(message + "\n");
-  }
-}
