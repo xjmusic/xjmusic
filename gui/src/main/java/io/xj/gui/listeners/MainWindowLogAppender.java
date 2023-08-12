4722d2abbd415fef8085313020472cf20c6a6ba7
Charney <charneykaye@gmail.com>
2023-08-11 20:02:26 -0700

    Stub of main window log appender
    [skip ci]

---
+++ gui/src/main/java/io/xj/gui/listeners/MainWindowLogAppender.java
@@ -0,0 +1,8 @@
+package io.xj.gui.listeners;
+
+public class MainWindowLogAppender {
+  public static void append(String message) {
+    var butts=123;// todo
+    // todo textArea.appendText(message + "\n");
+  }
+}
