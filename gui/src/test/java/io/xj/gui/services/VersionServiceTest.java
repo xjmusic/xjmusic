5b9304281afacb69ce0987f6f431f131c13c83aa
Charney Kaye <charneykaye@gmail.com>
2023-09-08 00:20:32 -0700

    Trying to get off this struggle bus

--- gui/src/test/java/io/xj/gui/services/VersionServiceTest.java
+++ gui/src/test/java/io/xj/gui/services/VersionServiceTest.java
@@ -4,7 +4,13 @@ package io.xj.gui.services;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.Resource;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,7 +21,69 @@ class VersionServiceTest {
 
   @BeforeEach
   void setUp() {
-    subject = new VersionService();
+    // load resource from "version.properties" file
+    Resource versionProperties = new Resource() {
+      @Override
+      public boolean exists() {
+        return true;
+      }
+
+      @Override
+      public boolean isReadable() {
+        return true;
+      }
+
+      @Override
+      public boolean isOpen() {
+        return false;
+      }
+
+      @Override
+      public URL getURL() throws IOException {
+        return null;
+      }
+
+      @Override
+      public URI getURI() throws IOException {
+        return null;
+      }
+
+      @Override
+      public File getFile() throws IOException {
+        return null;
+      }
+
+      @Override
+      public long contentLength() throws IOException {
+        return 0;
+      }
+
+      @Override
+      public long lastModified() throws IOException {
+        return 0;
+      }
+
+      @Override
+      public Resource createRelative(String relativePath) throws IOException {
+        return null;
+      }
+
+      @Override
+      public String getFilename() {
+        return "version.properties";
+      }
+
+      @Override
+      public String getDescription() {
+        return null;
+      }
+
+      @Override
+      public InputStream getInputStream() throws IOException {
+        return getClass().getClassLoader().getResourceAsStream("version.properties");
+      }
+    };
+    subject = new VersionService(versionProperties);
     subject.init();
   }
 
