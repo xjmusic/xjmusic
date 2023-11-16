9cb021eb4e6889741963a7bb055e3347b224b504
Charney Kaye <charneykaye@gmail.com>
2023-11-16 12:24:44 -0800

    Send bytes directly to line out for real-time playback

--- nexus/src/main/java/io/xj/nexus/ship/broadcast/StreamPlayerImpl.java
+++ nexus/src/main/java/io/xj/nexus/ship/broadcast/StreamPlayerImpl.java
@@ -6,25 +6,22 @@ package io.xj.nexus.ship.broadcast;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sound.sampled.*;
-import java.nio.ByteBuffer;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class StreamPlayerImpl implements StreamPlayer {
   static final Logger LOG = LoggerFactory.getLogger(StreamPlayer.class);
-  static final String THREAD_NAME = "StreamPlayer";
   SourceDataLine line;
-  final ConcurrentLinkedQueue<ByteBuffer> queue;
   final AtomicBoolean running = new AtomicBoolean(true);
 
   public StreamPlayerImpl(
     AudioFormat format
   ) {
-    queue = new ConcurrentLinkedQueue<>();
-
     try {
       DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
       if (!AudioSystem.isLineSupported(info)) {
@@ -32,28 +29,10 @@ public class StreamPlayerImpl implements StreamPlayer {
         line = null;
         return;
       }
-
       line = (SourceDataLine) AudioSystem.getLine(info);
       line.open(format);
       line.start();
       LOG.info("Did open audio system line out: {}", info);
-
-      CompletableFuture.supplyAsync(() -> {
-        final Thread currentThread = Thread.currentThread();
-        final String oldName = currentThread.getName();
-        currentThread.setName(THREAD_NAME);
-        try {
-          while (running.get()) {
-            var bytes = queue.poll();
-            if (Objects.isNull(bytes)) continue;
-            LOG.debug("Playing next {} bytes", bytes.array().length);
-            line.write(bytes.array(), 0, bytes.array().length);
-          }
-        } finally {
-          currentThread.setName(oldName);
-        }
-        return false;
-      });
 
     } catch (LineUnavailableException e) {
       LOG.error("Failed to initialize!", e);
@@ -66,7 +45,7 @@ public class StreamPlayerImpl implements StreamPlayer {
     if (!running.get())
       return samples;
 
-    queue.add(ByteBuffer.wrap(samples));
+    line.write(samples, 0, samples.length);
     return samples;
 
   }
