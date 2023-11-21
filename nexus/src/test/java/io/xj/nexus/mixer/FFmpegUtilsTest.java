33cfa54fd0d939f110b334239dc223760d3647fc
Charney <charneykaye@gmail.com>
2023-11-20 16:34:51 -0800

    Fix resampled audio for test

--- nexus/src/test/java/io/xj/nexus/mixer/FFmpegUtilsTest.java
+++ nexus/src/test/java/io/xj/nexus/mixer/FFmpegUtilsTest.java
@@ -3,7 +3,6 @@
 package io.xj.nexus.mixer;
 
 import io.xj.nexus.util.InternalResource;
-import io.xj.hub.util.ValueException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -16,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class FFmpegUtilsTest {
 
   @Test
-  void resampleAudio() throws IOException, ValueException {
+  void resampleAudio() throws IOException {
     String inputAudioFilePath = new InternalResource("/demo_reference_outputs/48000Hz_Signed_32bit_2ch.wav").getFile().getAbsolutePath();
     String outputAudioFilePath = Files.createTempFile("ffmpeg-test-resample-output", ".wav").toAbsolutePath().toString();
 
@@ -26,6 +25,6 @@ class FFmpegUtilsTest {
     InternalResource resource = new InternalResource("/test_audio_resampled/ffmpeg-test-resample-output-22000-8bit-1ch.wav");
     byte[] generatedFileBytes = Files.readAllBytes(generatedFilePath);
     byte[] testFileBytes = Files.readAllBytes(resource.getFile().toPath());
-    assertArrayEquals(testFileBytes, generatedFileBytes);
+    assertArrayEquals(testFileBytes, generatedFileBytes, String.format("Generated file %s does not match test file %s", generatedFilePath, resource.getFile().getAbsolutePath()));
   }
 }
