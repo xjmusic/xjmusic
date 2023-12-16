ccd0e3872e68c1ee3e864a1a86f610769f416340
Charney Kaye <charneykaye@gmail.com>
2023-12-16 13:57:28 -0800

    Fix oversight where retrieving segments from manager was done in list style instead of reading from map

--- nexus/src/main/java/io/xj/nexus/persistence/SegmentManagerImpl.java
+++ nexus/src/main/java/io/xj/nexus/persistence/SegmentManagerImpl.java
@@ -259,13 +259,10 @@ public class SegmentManagerImpl implements SegmentManager {
   @Override
   public List<Segment> readManyFromToOffset(int fromOffset, int toOffset) throws ManagerFatalException {
     try {
-      if (store.getAllSegments().isEmpty()
-        || toOffset < fromOffset
-        || fromOffset >= store.getAllSegments().size()
-        || fromOffset < 0)
-        return new ArrayList<>();
-
-      return new ArrayList<>(store.getAllSegments().subList(fromOffset, Math.min(store.getAllSegments().size(), toOffset + 1)));
+      return store.getAllSegments()
+        .stream()
+        .filter(s -> s.getId() >= fromOffset && s.getId() <= toOffset)
+        .toList();
 
     } catch (NexusException e) {
       throw new ManagerFatalException(e);
