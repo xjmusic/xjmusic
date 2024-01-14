91d275ec8f8837abefd18ac7cd2b9733ad0aeedf
Charney <charneykaye@gmail.com>
2024-01-14 09:56:08 -0800

    Checkpoint

--- gui/src/main/java/io/xj/gui/controllers/ContentBrowserController.java
+++ gui/src/main/java/io/xj/gui/controllers/ContentBrowserController.java
@@ -4,25 +4,15 @@ package io.xj.gui.controllers;
 
 import io.xj.gui.services.ProjectService;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ContentContainerController implements ReadyAfterBootController {
+public class ContentBrowserController implements ReadyAfterBootController {
   private final ProjectService projectService;
 
-  @FXML
-  protected BorderPane container;
-
-  @FXML
-  protected StackPane startupContainer;
-
-  @FXML
-  protected ContentBrowserController contentBrowser;
-
-  public ContentContainerController(
+  public ContentBrowserController(
     ProjectService projectService
   ) {
     this.projectService = projectService;
@@ -30,16 +20,11 @@ public class ContentContainerController implements ReadyAfterBootController {
 
   @Override
   public void onStageReady() {
-    contentBrowser.onStageReady();
-
-    startupContainer.visibleProperty().bind(projectService.getIsStateStandby());
-    startupContainer.managedProperty().bind(projectService.getIsStateStandby());
+    // TODO on stage ready
   }
 
   @Override
   public void onStageClose() {
-    contentBrowser.onStageClose();
-
     // FUTURE: close sub controllers
   }
 
