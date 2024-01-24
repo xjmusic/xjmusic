cfc72dc7d91893ac59c76d367165a9f54be5a13f
Charney Kaye <charneykaye@gmail.com>
2024-01-23 23:24:37 -0800

    Checkpoint

--- nexus/src/test/java/io/xj/nexus/project/ProjectManagerImplTest.java
+++ nexus/src/test/java/io/xj/nexus/project/ProjectManagerImplTest.java
@@ -5,20 +5,43 @@ import io.xj.hub.entity.EntityFactoryImpl;
 import io.xj.hub.json.JsonProvider;
 import io.xj.hub.json.JsonProviderImpl;
 import io.xj.nexus.http.HttpClientProvider;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Objects;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProjectManagerImplTest {
-  ProjectManager subject;
+  private final String pathToProjectFile;
+  private final String baseDir;
+  private ProjectManager subject;
 
   @Mock
   HttpClientProvider httpClientProvider;
+
+  @Mock
+  CloseableHttpClient httpClient;
+
+  public ProjectManagerImplTest() throws URISyntaxException {
+    baseDir = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("project")).toURI()).getAbsolutePath();
+    pathToProjectFile = baseDir + File.separator + "test-project.xj";
+  }
 
   @BeforeEach
   void setUp() {
@@ -29,29 +52,61 @@ class ProjectManagerImplTest {
 
   @Test
   void openProjectFromLocalFile() {
-    var testProjectResource = getClass().getResource("/test-project.xj");
-    assertNotNull(testProjectResource);
+    assertTrue(subject.openProjectFromLocalFile(pathToProjectFile));
 
-    subject.openProjectFromLocalFile(testProjectResource.getPath());
+    assertEquals(UUID.fromString("23bcaded-2186-4697-912e-5f47bae9e9a0"), subject.getContent().getProjects().stream().findFirst().orElseThrow().getId());
+    assertEquals(2, subject.getContent().getInstruments().size());
+    assertEquals(2, subject.getContent().getPrograms().size());
+    assertEquals(2, subject.getContent().getInstrumentMemes().size());
+    assertEquals(2, subject.getContent().getInstrumentAudios().size());
+    assertEquals(2, subject.getContent().getProgramMemes().size());
+    assertEquals(2, subject.getContent().getProgramSequences().size());
+    assertEquals(2, subject.getContent().getProgramSequencePatterns().size());
+    assertEquals(2, subject.getContent().getProgramVoices().size());
+    assertEquals(2, subject.getContent().getProgramVoiceTracks().size());
+    assertEquals(2, subject.getContent().getProgramSequenceBindings().size());
+    assertEquals(2, subject.getContent().getProgramSequencePatternEvents().size());
+    assertEquals(2, subject.getContent().getProgramSequenceBindingMemes().size());
   }
 
   @Test
   void getProjectPathPrefix() {
-    // TODO test ProjectManagerImpl.getProjectPathPrefix(..)
+    assertTrue(subject.openProjectFromLocalFile(pathToProjectFile));
+
+    assertEquals(baseDir + File.separator, subject.getProjectPathPrefix());
   }
 
   @Test
   void getProject() {
-    // TODO test ProjectManagerImpl.getProject(..)
+    assertTrue(subject.openProjectFromLocalFile(pathToProjectFile));
+
+    assertEquals(UUID.fromString("23bcaded-2186-4697-912e-5f47bae9e9a0"), subject.getProject().orElseThrow().getId());
   }
 
   @Test
   void getAudioBaseUrl() {
-    // TODO test ProjectManagerImpl.getAudioBaseUrl(..)
+    subject.cloneProjectFromDemoTemplate("https://audio.test.xj.io/", "test", "test", "test");
+
+    assertEquals("https://audio.test.xj.io/", subject.getAudioBaseUrl());
   }
 
   @Test
-  void cloneProjectFromDemoTemplate() {
+  void cloneProjectFromDemoTemplate() throws IOException {
+    when(httpClientProvider.getClient()).thenReturn(httpClient);
+//    when(httpClient.execute(any(HttpHead.class))).thenReturn();
+    doAnswer(invocation -> {
+      Object[] args = invocation.getArguments();
+      HttpUriRequest request = (HttpUriRequest) args[0];
+
+      if (true == true) {
+        return null;
+      } else {
+        return null;
+      }
+    }).when(httpClient).execute(any(HttpHead.class));
+
+    subject.cloneProjectFromDemoTemplate("https://audio.test.xj.io/", "test", "test", "test");
+
     // TODO test ProjectManagerImpl.cloneProjectFromDemoTemplate(..)
   }
 
