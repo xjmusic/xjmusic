466ce4fde287188f1cf8152bac2b3a8c27fc4966
Jemo <jamesshisia6@gmail.com>
2024-02-03 03:31:04 +0300

    Program config UI and controller settings

---
+++ gui/src/main/java/io/xj/gui/controllers/content/program/ProgramConfigController.java
@@ -0,0 +1,57 @@
+package io.xj.gui.controllers.content.program;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class ProgramConfigController {
+  @FXML
+  public TextArea configTextArea;
+  @FXML
+  public Button cancelButton;
+  @FXML
+  public HBox saveAndCancelButtonsContainer;
+  @FXML
+  public Button cancelConfigChanges;
+  @FXML
+  public Button saveConfigChanges;
+  private String originalText = "";
+  private final SimpleBooleanProperty visibleProperty = new SimpleBooleanProperty(false);
+
+  protected void programConfigInitializer(ProgramEditorController programEditorController, Stage stage) {
+    originalText = programEditorController.config.get();
+    configTextArea.setText(programEditorController.config.get());
+    cancelButton.setOnAction(e -> stage.close());
+    cancelConfigChanges.setOnAction(e -> stage.close());
+    saveConfigChanges.setOnAction(e -> {
+      programEditorController.config.set(configTextArea.getText());
+      stage.close();
+    });
+    // Bind the visibility of the Button and HBox to the BooleanProperty
+    saveAndCancelButtonsContainer.visibleProperty().bind(visibleProperty);
+    cancelButton.visibleProperty().bind(visibleProperty.not());
+    closeWindowOnClickingAway(stage);
+    detectConfigDataChanges();
+  }
+
+  private void detectConfigDataChanges() {
+    // Add a listener to detect text changes
+    configTextArea.textProperty().addListener((observable, oldValue, newValue) -> {
+      visibleProperty.set(!newValue.equals(originalText));
+    });
+  }
+
+  /**
+   * closes the stage when clicking outside it
+   */
+  public static void closeWindowOnClickingAway(Stage window) {
+    window.focusedProperty().addListener((obs, oldValue, newValue) -> {
+      if (!newValue) {
+        window.close();
+      }
+    });
+  }
+}
