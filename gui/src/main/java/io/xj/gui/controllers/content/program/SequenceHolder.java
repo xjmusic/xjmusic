b7a97c7872a826685f3707372c88ce451b8baf7f
Jemo <jamesshisia6@gmail.com>
2024-02-11 19:29:21 +0300

    show sequence selector UI on click in the middle of the individual screen. Uses screen size to align

--- gui/src/main/java/io/xj/gui/controllers/content/program/SequenceHolder.java
+++ gui/src/main/java/io/xj/gui/controllers/content/program/SequenceHolder.java
@@ -1,14 +1,18 @@
 package io.xj.gui.controllers.content.program;
 
+import io.xj.gui.services.ProjectService;
+import io.xj.gui.services.ThemeService;
 import io.xj.hub.util.StringUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +23,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.UUID;
+
+import static io.xj.gui.controllers.content.program.ProgramEditorController.closeWindowOnClickingAway;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -29,44 +36,65 @@ public class SequenceHolder {
   public Label offSet;
   @FXML
   public Button addSequenceButton;
-  @Value("classpath:/views/content/program/sequence-item-bind-mode.fxml")
-  private Resource sequenceItemBindingFxml;
+  @Value("classpath:/views/content/program/create-binding-item.fxml")
+  private Resource createBindingFxml;
   private final ProgramEditorController programEditorController;
   private final ApplicationContext applicationContext;
+  private final ProjectService projectService;
   private final Logger LOG = LoggerFactory.getLogger(SequenceHolder.class);
+  private final ThemeService themeService;
   private HBox bindViewParentContainer;
   private int position;
 
-  public void setUp(HBox bindViewParentContainer, int position) {
+  public SequenceHolder(ApplicationContext applicationContext, ProgramEditorController programEditorController,
+                        ProjectService projectService, ThemeService themeService) {
+    this.programEditorController = programEditorController;
+    this.applicationContext = applicationContext;
+    this.projectService = projectService;
+    this.themeService = themeService;
+  }
+
+  public void setUp(HBox bindViewParentContainer, int position, UUID programId,UUID programSequenceId) {
     this.position = position;
     this.bindViewParentContainer = bindViewParentContainer;
     offSet.setText(String.valueOf(position - 1));
-    addSequenceButton.setOnAction(e -> addSequenceItem());
+    addSequenceButton.setOnAction(e -> showSequenceBindingUI(programId,programSequenceId));
   }
 
-  public SequenceHolder(ApplicationContext applicationContext, ProgramEditorController programEditorController) {
-    this.programEditorController = programEditorController;
-    this.applicationContext = applicationContext;
-  }
-
-  private void addSequenceItem() {
+  protected void showSequenceBindingUI(UUID programId, UUID programSequenceId) {
     try {
-      FXMLLoader loader = new FXMLLoader(sequenceItemBindingFxml.getURL());
+      var sequences = projectService.getContent().getSequencesOfProgram(programId);
+      Stage stage = new Stage(StageStyle.TRANSPARENT);
+      FXMLLoader loader = new FXMLLoader(createBindingFxml.getURL());
       loader.setControllerFactory(applicationContext::getBean);
       Parent root = loader.load();
-      sequenceHolder.getChildren().add(sequenceHolder.getChildren().size() - 1, root);
-      VBox.setMargin(root, new Insets(0, 5, 0, 5));
-      SequenceItemBindMode sequenceItemBindMode = loader.getController();
-      sequenceItemBindMode.setUp(sequenceHolder, root, bindViewParentContainer, position);
-      checkIfNextItemIsPresent();
+      CreateBindingItem createBindingItem = loader.getController();
+      createBindingItem.setUp(sequences,bindViewParentContainer,sequenceHolder,position,programId,programSequenceId);
+      stage.setScene(new Scene(root));
+      // Set the owner of the stage
+      stage.initOwner(themeService.getMainScene().getWindow());
+      stage.show();
+      closeWindowOnClickingAway(stage);
+      centerOnScreen(stage);
     } catch (IOException e) {
-      LOG.error("Error creating new Sequence \n{}", StringUtils.formatStackTrace(e), e);
+      LOG.error("Error opening Sequence Search window!\n{}", StringUtils.formatStackTrace(e), e);
     }
   }
 
-  private void checkIfNextItemIsPresent() {
-    if (bindViewParentContainer.getChildren().size() - 1 < position + 1) {
-      programEditorController.addBindingView(position + 1);
-    }
+  // Method to center a UI element on the screen
+  public static void centerOnScreen(Stage stage) {
+    // Get the screen dimensions
+    double screenWidth = stage.getOwner().getWidth(); // or Screen.getPrimary().getVisualBounds().getWidth()
+    double screenHeight = stage.getOwner().getHeight(); // or Screen.getPrimary().getVisualBounds().getHeight()
+
+    // Calculate the position of the stage
+    double stageWidth = stage.getWidth();
+    double stageHeight = stage.getHeight();
+    double x = (screenWidth - stageWidth) / 2;
+    double y = (screenHeight - stageHeight) / 2;
+
+    // Set the stage position
+    stage.setX(x);
+    stage.setY(y);
   }
 }
