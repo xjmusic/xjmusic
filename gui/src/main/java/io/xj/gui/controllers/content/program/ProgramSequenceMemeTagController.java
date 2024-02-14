bd4e45cb140584869cbc3c96fc5e21bb41ca2104
Jemo <jamesshisia6@gmail.com>
2024-02-14 03:14:13 +0300

    customized the meme tag

--- gui/src/main/java/io/xj/gui/controllers/content/program/ProgramSequenceMemeTagController.java
+++ gui/src/main/java/io/xj/gui/controllers/content/program/ProgramSequenceMemeTagController.java
@@ -8,8 +8,10 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -33,15 +36,24 @@ public class ProgramSequenceMemeTagController {
   private final SimpleStringProperty name = new SimpleStringProperty("");
   static final Logger LOG = LoggerFactory.getLogger(MemeTagController.class);
   private final ProjectService projectService;
+  @FXML
+  public AnchorPane memeParent;
   private ProgramSequenceBindingMeme currentMeme;
   private UUID sequenceBindingId;
+  private VBox sequenceHolder;
+  private AnchorPane parentAnchorPane;
+  private HBox memeHolder;
+
   public ProgramSequenceMemeTagController(ProjectService projectService) {
     this.projectService = projectService;
   }
 
-  public void setUp(Parent root, ProgramSequenceBindingMeme meme, UUID sequenceBindingId, HBox memeHolder) {
+  public void setUp(Parent root, ProgramSequenceBindingMeme meme, UUID sequenceBindingId, HBox memeHolder, VBox sequenceHolder, AnchorPane parentAnchorPane) {
+    this.sequenceHolder = sequenceHolder;
+    this.parentAnchorPane = parentAnchorPane;
     this.currentMeme = meme;
     this.sequenceBindingId = sequenceBindingId;
+    this.memeHolder=memeHolder;
     name.set(meme.getName());
     memeNameLabel.setText(name.get());
     memeNameField.setText(name.get());
@@ -52,7 +64,7 @@ public class ProgramSequenceMemeTagController {
     projectService.getContent().getMemesOfSequenceBinding(sequenceBindingId);
     toggleVisibility();
     setMemeTextProcessing();
-    deleteMemeButton.setOnAction(e -> deleteMemeTag(root,memeHolder));
+    deleteMemeButton.setOnAction(e -> deleteMemeTag(root, memeHolder));
   }
 
   private void updateMeme() {
@@ -94,12 +106,44 @@ public class ProgramSequenceMemeTagController {
       projectService.getContent().getProgramSequenceBindings().removeIf(meme -> meme.getId().equals(currentMeme.getId()));
       //notify modification
       projectService.isModifiedProperty().set(true);
-      //remove the meme from UI
+      // Remove the child node from memeHolder
       memeHolder.getChildren().remove(root);
-      LOG.info("Deleted " + currentMeme.getName());
+      // Assuming memeHolder and memeParent are valid nodes
+      if (isLastItemWithChildren() || isWithMoreChildren()) {
+        sequenceHolder.setPrefWidth(sequenceHolder.getWidth() - memeParent.getPrefWidth());
+      }
+
     } catch (Exception e) {
       LOG.error("Failed to delete Meme " + Arrays.toString(e.getStackTrace()) + " !!");
     }
+  }
+
+  private boolean isLastItemWithChildren() {
+    AtomicBoolean isLastItemWithChildren = new AtomicBoolean(true);
+    sequenceHolder.getChildren().forEach(node -> {
+      if (node instanceof AnchorPane && node != parentAnchorPane) {
+        ((AnchorPane) node).getChildren().forEach(anchorPaneChild -> {
+          if (anchorPaneChild instanceof HBox && ((HBox) anchorPaneChild).getChildren().size() > 0) {
+            isLastItemWithChildren.set(false);
+          }
+        });
+      }
+    });
+    return isLastItemWithChildren.get();
+  }
+
+  private boolean isWithMoreChildren() {
+    AtomicBoolean isWithMoreChildren = new AtomicBoolean(true);
+    sequenceHolder.getChildren().forEach(node -> {
+      if (node instanceof AnchorPane && node != parentAnchorPane) {
+        ((AnchorPane) node).getChildren().forEach(anchorPaneChild -> {
+          if (anchorPaneChild instanceof HBox && ((HBox) anchorPaneChild).getChildren().size() > memeHolder.getChildren().size()) {
+            isWithMoreChildren.set(false);
+          }
+        });
+      }
+    });
+    return isWithMoreChildren.get();
   }
 
   private double computeTextWidth(javafx.scene.text.Font font, String text) {
