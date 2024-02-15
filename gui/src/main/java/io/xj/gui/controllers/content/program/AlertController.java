1cc4afac80197c00c1f496296492544249a0ab7a
Jemo <jamesshisia6@gmail.com>
2024-02-15 21:16:39 +0300

    alert

---
+++ gui/src/main/java/io/xj/gui/controllers/content/program/AlertController.java
@@ -0,0 +1,21 @@
+package io.xj.gui.controllers.content.program;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+
+public class AlertController {
+  @FXML
+  public Label titleLabel;
+  @FXML
+  public Label messageLabel;
+  @FXML
+  public AnchorPane container;
+
+  public void setUp(String title, String message, String color){
+    titleLabel.setText(title);
+    messageLabel.setText(message);
+    container.setStyle("-fx-background-color:"+color);
+  }
+}
