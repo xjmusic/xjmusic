3183fbb356787c1bb6f6dfda00eeaa07089e5dfa
Jemo <jamesshisia6@gmail.com>
2024-02-20 01:49:53 +0300

    Add new ProgramSequencePattern

--- gui/src/main/java/io/xj/gui/controllers/content/program/PatternMenuController.java
+++ gui/src/main/java/io/xj/gui/controllers/content/program/PatternMenuController.java
@@ -1,13 +1,18 @@
 package io.xj.gui.controllers.content.program;
 
 import io.xj.gui.services.ProjectService;
+import io.xj.hub.tables.pojos.ProgramSequencePattern;
 import io.xj.hub.tables.pojos.ProgramVoice;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -20,17 +25,35 @@ public class PatternMenuController {
     @FXML
     public Button clonePattern;
     private ProgramVoice voice;
+    private final ProgramEditorController programEditorController;
+    static final Logger LOG = LoggerFactory.getLogger(PatternMenuController.class);
 
-    public PatternMenuController(ProjectService projectService) {
+    public PatternMenuController(ProjectService projectService, ProgramEditorController programEditorController) {
+        this.programEditorController = programEditorController;
         this.projectService = projectService;
     }
 
     public void setUp(Parent root, ProgramVoice voice) {
-        this.voice=voice;
-        newPattern.setOnAction(e->createVoicePattern());
+        this.voice = voice;
+        newPattern.setOnAction(e -> createVoicePattern());
+        deletePattern.setOnAction(e->deletePattern());
     }
 
-    private void createVoicePattern(){
-//        projectService.getContent().put();
+    private void createVoicePattern() {
+       try {
+           ProgramSequencePattern newPattern = new ProgramSequencePattern(UUID.randomUUID(), programEditorController.getProgramId(), programEditorController.getSequenceId(), voice.getId(), "XXX",
+                   (short) 4);
+           projectService.getContent().put(newPattern);
+       } catch (Exception e) {
+           LOG.error("Cannot create new ProgramSequencePattern");
+       }
+    }
+
+    private void deletePattern() {
+        try {
+//            projectService.deleteContent();
+        } catch (Exception e) {
+            LOG.error("Cannot create new ProgramSequencePattern");
+        }
     }
 }
