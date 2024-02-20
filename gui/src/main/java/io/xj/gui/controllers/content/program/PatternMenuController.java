4eecb866f74332618da1bb433d323c08f737ecd6
Jemo <jamesshisia6@gmail.com>
2024-02-20 22:58:52 +0300

    create a ProgramSequencePattern delete a ProgramSequencePattern Update  UI after delete and create operations

--- gui/src/main/java/io/xj/gui/controllers/content/program/PatternMenuController.java
+++ gui/src/main/java/io/xj/gui/controllers/content/program/PatternMenuController.java
@@ -5,6 +5,7 @@ import io.xj.hub.tables.pojos.ProgramSequencePattern;
 import io.xj.hub.tables.pojos.ProgramVoice;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,34 +27,48 @@ public class PatternMenuController {
     public Button clonePattern;
     private ProgramVoice voice;
     private final ProgramEditorController programEditorController;
+    private VoiceController voiceController;
     static final Logger LOG = LoggerFactory.getLogger(PatternMenuController.class);
 
-    public PatternMenuController(ProjectService projectService, ProgramEditorController programEditorController) {
+    private ProgramSequencePattern activeProgramSequencePattern;
+
+    public PatternMenuController(ProjectService projectService, ProgramEditorController programEditorController
+    ) {
         this.programEditorController = programEditorController;
         this.projectService = projectService;
     }
 
-    public void setUp(Parent root, ProgramVoice voice) {
+    public void setUp(Parent root, ProgramVoice voice, ProgramSequencePattern programSequencePattern, VoiceController voiceController) {
         this.voice = voice;
+        this.voiceController = voiceController;
+        activeProgramSequencePattern = programSequencePattern;
         newPattern.setOnAction(e -> createVoicePattern());
-        deletePattern.setOnAction(e->deletePattern());
+        deletePattern.setOnAction(e -> deletePattern());
     }
 
     private void createVoicePattern() {
-       try {
-           ProgramSequencePattern newPattern = new ProgramSequencePattern(UUID.randomUUID(), programEditorController.getProgramId(), programEditorController.getSequenceId(), voice.getId(), "XXX",
-                   (short) 4);
-           projectService.getContent().put(newPattern);
-       } catch (Exception e) {
-           LOG.error("Cannot create new ProgramSequencePattern");
-       }
+        try {
+            ProgramSequencePattern newPattern = new ProgramSequencePattern(UUID.randomUUID(), programEditorController.getProgramId(), programEditorController.getSequenceId(), voice.getId(), "XXX",
+                    (short) 4);
+            projectService.getContent().put(newPattern);
+            voiceController.getProgramSequencePatternsOfThisVoice().add(newPattern);
+            voiceController.updatePatternUI(newPattern);
+            voiceController.setSelectedProgramSequencePattern(newPattern);
+            projectService.showAlert(Alert.AlertType.INFORMATION, "", "Created", "New Pattern");
+        } catch (Exception e) {
+            LOG.error("Cannot create new ProgramSequencePattern");
+        }
     }
 
     private void deletePattern() {
         try {
-//            projectService.deleteContent();
+            projectService.deleteContent(activeProgramSequencePattern);
+            voiceController.getProgramSequencePatternsOfThisVoice().remove(activeProgramSequencePattern);
+            voiceController.setSelectedProgramSequencePattern(voiceController.getProgramSequencePatternsOfThisVoice().isEmpty() ?
+                    null : voiceController.getProgramSequencePatternsOfThisVoice().get(0));
+            voiceController.updatePatternUI(voiceController.getSelectedProgramSequencePattern());
         } catch (Exception e) {
-            LOG.error("Cannot create new ProgramSequencePattern");
+            LOG.error("Failed to delete ProgramSequencePattern ");
         }
     }
 }
