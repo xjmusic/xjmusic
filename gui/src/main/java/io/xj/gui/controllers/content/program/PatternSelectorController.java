9a4e25894678b19aedcbcf19cc47e1203b027ae4
Jemo <jamesshisia6@gmail.com>
2024-02-20 23:10:00 +0300

    Code cleanup. correct indentation at SequenceBindingItemCreationController. Passed the VoiceController via setUp method. Show ProgramSequencePatterns list in combobox on load. Select a ProgramSequencePattern from the combobox and dynamically update the UI with the ProgramSequencePattern values.

--- gui/src/main/java/io/xj/gui/controllers/content/program/PatternSelectorController.java
+++ gui/src/main/java/io/xj/gui/controllers/content/program/PatternSelectorController.java
@@ -1,13 +1,13 @@
 package io.xj.gui.controllers.content.program;
 
 import io.xj.gui.services.ProjectService;
-import io.xj.hub.tables.pojos.ProgramSequence;
 import io.xj.hub.tables.pojos.ProgramSequencePattern;
 import io.xj.hub.tables.pojos.ProgramVoice;
-import javafx.collections.ObservableList;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import org.controlsfx.control.SearchableComboBox;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -15,45 +15,44 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public class PatternSearchController {
+public class PatternSelectorController {
     @FXML
 
     public VBox container;
     @FXML
-    public SearchableComboBox<ProgramSequencePattern> patternSearch;
+    public SearchableComboBox<ProgramSequencePattern> patternSelector;
     private final ProjectService projectService;
     private final ProgramEditorController programEditorController;
-    private ProgramSequencePattern programSequencePattern;
-
+    private Collection<ProgramSequencePattern> programSequencePatternsOfVoice;
     private ProgramVoice voice;
+    private  VoiceController voiceController;
 
-    public PatternSearchController(ProjectService projectService,
-                                   ProgramEditorController programEditorController){
-        this.projectService=projectService;
-        this.programEditorController=programEditorController;
+    public PatternSelectorController(ProjectService projectService,
+                                   ProgramEditorController programEditorController) {
+        this.projectService = projectService;
+        this.programEditorController = programEditorController;
     }
-    public void setUp(ProgramSequencePattern programSequencePattern, ProgramVoice voice) {
-        this.voice=voice;
-        this.programSequencePattern=programSequencePattern;
-        selectPassedSequence();
+
+    public void setUp(Collection<ProgramSequencePattern> programSequencePatternsOfVoice, ProgramVoice voice,VoiceController voiceController,Stage stage) {
+        this.voice = voice;
+        this.voiceController=voiceController;
+        this.programSequencePatternsOfVoice = programSequencePatternsOfVoice;
         setCombobox();
-        // Request focus and show the ComboBox
-        patternSearch.requestFocus();
-        patternSearch.show();
-    }
+        selectProgramSequencePattern(stage);
 
+    }
 
     private void setCombobox() {
         // Clear existing items
-        patternSearch.getItems().clear();
-        Collection<ProgramSequencePattern> programSequencePatterns=projectService.getContent().getPatternsOfSequence(programEditorController.getSequenceId());
-        patternSearch.getItems().addAll(programSequencePatterns);
+        patternSelector.getItems().clear();
+        patternSelector.getItems().addAll(programSequencePatternsOfVoice);
 
         // Set the cell factory to display the name of ProgramSequence
-        patternSearch.setCellFactory(param -> new ListCell<>() {
+        patternSelector.setCellFactory(param -> new ListCell<>() {
             @Override
             protected void updateItem(ProgramSequencePattern item, boolean empty) {
                 super.updateItem(item, empty);
@@ -66,7 +65,7 @@ public class PatternSearchController {
         });
 
         // Set the string converter to get ProgramSequence from its name
-        patternSearch.setConverter(new StringConverter<>() {
+        patternSelector.setConverter(new StringConverter<>() {
             @Override
             public String toString(ProgramSequencePattern object) {
                 return object != null ? object.getName() : "";
@@ -74,24 +73,24 @@ public class PatternSearchController {
 
             @Override
             public ProgramSequencePattern fromString(String string) {
-                return programSequencePatterns.stream()
+                return programSequencePatternsOfVoice.stream()
                         .filter(programSequencePattern -> programSequencePattern.getName().equals(string))
                         .findFirst()
                         .orElse(null);
             }
         });
-
+        patternSelector.getSelectionModel().selectFirst();
+        voiceController.setSelectedProgramSequencePattern(patternSelector.getSelectionModel().getSelectedItem());
     }
 
-    private void selectPassedSequence() {
-        if (programSequencePattern != null) {
-            ObservableList<ProgramSequencePattern> items = patternSearch.getItems();
-            for (ProgramSequencePattern item : items) {
-                if (item.getId().equals(programSequencePattern.getId())) {
-                    patternSearch.getSelectionModel().select(item);
-                    break;
-                }
+    private void selectProgramSequencePattern(Stage stage) {
+        patternSelector.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                voiceController.setSelectedProgramSequencePattern(newValue);
+                voiceController.updatePatternUI(newValue);
+                Platform.runLater(stage::close);
             }
-        }
+        });
     }
+
 }
--- gui/src/main/java/io/xj/gui/controllers/content/program/SequenceBindingItemCreationController.java
+++ gui/src/main/java/io/xj/gui/controllers/content/program/SequenceBindingItemCreationController.java
@@ -28,119 +28,119 @@ import java.util.UUID;
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SequenceBindingItemCreationController {
-  @FXML
-  public VBox container;
-  @FXML
-  public SearchableComboBox<ProgramSequence> sequenceSearch;
-  private final Logger LOG = LoggerFactory.getLogger(SequenceSearchController.class);
-  private final ProjectService projectService;
-  @Value("classpath:/views/content/program/sequence-selector.fxml")
-  private Resource sequenceHolderFxml;
-  @Value("classpath:/views/content/program/sequence-binding-item.fxml")
-  private Resource sequenceItemBindingFxml;
-  private HBox bindViewParentContainer;
-  private VBox sequenceSelector;
-  private final ApplicationContext applicationContext;
-  private final ProgramEditorController programEditorController;
-  private int position;
+    @FXML
+    public VBox container;
+    @FXML
+    public SearchableComboBox<ProgramSequence> sequenceSearch;
+    private final Logger LOG = LoggerFactory.getLogger(SequenceSearchController.class);
+    private final ProjectService projectService;
+    @Value("classpath:/views/content/program/sequence-selector.fxml")
+    private Resource sequenceHolderFxml;
+    @Value("classpath:/views/content/program/sequence-binding-item.fxml")
+    private Resource sequenceItemBindingFxml;
+    private HBox bindViewParentContainer;
+    private VBox sequenceSelector;
+    private final ApplicationContext applicationContext;
+    private final ProgramEditorController programEditorController;
+    private int position;
 
-  public SequenceBindingItemCreationController(ProjectService projectService, ApplicationContext applicationContext
-    , ProgramEditorController programEditorController) {
-    this.projectService = projectService;
-    this.applicationContext = applicationContext;
-    this.programEditorController = programEditorController;
-  }
+    public SequenceBindingItemCreationController(ProjectService projectService, ApplicationContext applicationContext
+            , ProgramEditorController programEditorController) {
+        this.projectService = projectService;
+        this.applicationContext = applicationContext;
+        this.programEditorController = programEditorController;
+    }
 
-  public void setUp(HBox bindViewParentContainer, VBox sequenceSelector,
-                    int position, UUID programId) {
-    this.bindViewParentContainer = bindViewParentContainer;
-    this.sequenceSelector = sequenceSelector;
-    this.position = position;
-    setCombobox();
-    addSequenceBinding(position - 1, programId);
-  }
+    public void setUp(HBox bindViewParentContainer, VBox sequenceSelector,
+                      int position, UUID programId) {
+        this.bindViewParentContainer = bindViewParentContainer;
+        this.sequenceSelector = sequenceSelector;
+        this.position = position;
+        setCombobox();
+        addSequenceBinding(position - 1, programId);
+    }
 
-  private void addSequenceBinding(int offSet, UUID programId) {
-    sequenceSearch.valueProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue != null) {
-        addSequence(programId, newValue, offSet);
-        closeWindow();
-      }
-    });
-  }
+    private void addSequenceBinding(int offSet, UUID programId) {
+        sequenceSearch.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                addSequence(programId, newValue, offSet);
+                closeWindow();
+            }
+        });
+    }
 
-  private void setCombobox() {
-    // Clear existing items
-    sequenceSearch.getItems().clear();
+    private void setCombobox() {
+        // Clear existing items
+        sequenceSearch.getItems().clear();
 
-    sequenceSearch.getItems().addAll(programEditorController.programSequenceObservableList);
+        sequenceSearch.getItems().addAll(programEditorController.programSequenceObservableList);
 
-    // Set the cell factory to display the name of ProgramSequence
-    sequenceSearch.setCellFactory(param -> new ListCell<>() {
-      @Override
-      protected void updateItem(ProgramSequence item, boolean empty) {
-        super.updateItem(item, empty);
-        if (empty || item == null) {
-          setText(null);
-        } else {
-          setText(item.getName());
+        // Set the cell factory to display the name of ProgramSequence
+        sequenceSearch.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(ProgramSequence item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getName());
+                }
+            }
+        });
+
+        // Set the string converter to get ProgramSequence from its name
+        sequenceSearch.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(ProgramSequence object) {
+                return object != null ? object.getName() : "";
+            }
+
+            @Override
+            public ProgramSequence fromString(String string) {
+                return programEditorController.programSequenceObservableList.stream()
+                        .filter(sequence -> sequence.getName().equals(string))
+                        .findFirst()
+                        .orElse(null);
+            }
+        });
+    }
+
+    private void addSequence(UUID programId, ProgramSequence programSequence, int offSet) {
+        ProgramSequenceBinding programSequenceBinding = new ProgramSequenceBinding(UUID.randomUUID(), programId, programSequence.getId(), offSet);
+        addSequenceItem(programSequenceBinding, programSequence);
+    }
+
+    public void addSequenceItem(ProgramSequenceBinding programSequenceBinding, ProgramSequence programSequence) {
+        try {
+            ProgramEditorController.createProgramSequenceBindingItem(programSequenceBinding, sequenceSelector, position, programSequence, sequenceItemBindingFxml, applicationContext, bindViewParentContainer, projectService);
+            checkIfNextItemIsPresent();
+        } catch (Exception e) {
+            LOG.error("Error creating new Sequence \n{}", StringUtils.formatStackTrace(e), e);
         }
-      }
-    });
-
-    // Set the string converter to get ProgramSequence from its name
-    sequenceSearch.setConverter(new StringConverter<>() {
-      @Override
-      public String toString(ProgramSequence object) {
-        return object != null ? object.getName() : "";
-      }
-
-      @Override
-      public ProgramSequence fromString(String string) {
-        return programEditorController.programSequenceObservableList.stream()
-          .filter(sequence -> sequence.getName().equals(string))
-          .findFirst()
-          .orElse(null);
-      }
-    });
-
-  }
-
-  private void addSequence(UUID programId, ProgramSequence programSequence, int offSet) {
-    ProgramSequenceBinding programSequenceBinding = new ProgramSequenceBinding(UUID.randomUUID(), programId, programSequence.getId(), offSet);
-    addSequenceItem(programSequenceBinding,programSequence);
-  }
-
-  public void addSequenceItem(ProgramSequenceBinding programSequenceBinding, ProgramSequence programSequence) {
-    try {
-      ProgramEditorController.createProgramSequenceBindingItem(programSequenceBinding, sequenceSelector, position, programSequence, sequenceItemBindingFxml, applicationContext, bindViewParentContainer, projectService);
-      checkIfNextItemIsPresent();
-    } catch (Exception e) {
-      LOG.error("Error creating new Sequence \n{}", StringUtils.formatStackTrace(e), e);
     }
-  }
 
-  private void checkIfNextItemIsPresent() {
-    if (bindViewParentContainer.getChildren().size() - 1 < position + 1) {
-      addBindingView(position+1);
+    private void checkIfNextItemIsPresent() {
+        if (bindViewParentContainer.getChildren().size() - 1 < position + 1) {
+            addBindingView(position + 1);
+        }
     }
-  }
 
-  protected void addBindingView(int position) {
-    try {
-      FXMLLoader loader = new FXMLLoader(sequenceHolderFxml.getURL());
-      loader.setControllerFactory(applicationContext::getBean);
-      Parent root = loader.load();
-      bindViewParentContainer.getChildren().add(bindViewParentContainer.getChildren().size(), root);
-      SequenceSelectorController sequenceSelector = loader.getController();
-      sequenceSelector.setUp(bindViewParentContainer, position, programEditorController.getProgramId());
-      HBox.setHgrow(root, javafx.scene.layout.Priority.ALWAYS);
-    } catch (IOException e) {
-      LOG.error("Error loading Sequence Holder view!\n{}", StringUtils.formatStackTrace(e), e);
+    protected void addBindingView(int position) {
+        try {
+            FXMLLoader loader = new FXMLLoader(sequenceHolderFxml.getURL());
+            loader.setControllerFactory(applicationContext::getBean);
+            Parent root = loader.load();
+            bindViewParentContainer.getChildren().add(bindViewParentContainer.getChildren().size(), root);
+            SequenceSelectorController sequenceSelector = loader.getController();
+            sequenceSelector.setUp(bindViewParentContainer, position, programEditorController.getProgramId());
+            HBox.setHgrow(root, javafx.scene.layout.Priority.ALWAYS);
+        } catch (IOException e) {
+            LOG.error("Error loading Sequence Holder view!\n{}", StringUtils.formatStackTrace(e), e);
+        }
     }
-  }
-  private void closeWindow() {
-    Stage stage = (Stage) sequenceSearch.getScene().getWindow();
-    stage.close();
-  }
+
+    private void closeWindow() {
+        Stage stage = (Stage) sequenceSearch.getScene().getWindow();
+        stage.close();
+    }
 }
