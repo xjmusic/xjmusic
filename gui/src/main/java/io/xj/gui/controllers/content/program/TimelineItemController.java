91927e549c45bfe9623df5b82821ca9da877eaf0
Jemo <jamesshisia6@gmail.com>
2024-02-22 04:43:07 +0300

    dotted line. highlight item if it lies within the total value of the ProgramSequencePattern

--- gui/src/main/java/io/xj/gui/controllers/content/program/TimelineItemController.java
+++ gui/src/main/java/io/xj/gui/controllers/content/program/TimelineItemController.java
@@ -1,7 +1,9 @@
 package io.xj.gui.controllers.content.program;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,18 @@ import org.springframework.stereotype.Component;
 public class TimelineItemController {
     @FXML
     public AnchorPane timelineParent;
+    private final ProgramEditorController programEditorController;
+    @FXML
+    public Line middleLine;
 
-    public void setUp(int timelineItemId) {
+    public TimelineItemController(ProgramEditorController programEditorController) {
+        this.programEditorController = programEditorController;
+    }
+
+    public void setUp(int timelineItemId, VoiceController voiceController) {
+        middleLine.getStrokeDashArray().addAll(2d, 5d);
+        if (voiceController.getTotal() > 0 && timelineItemId <= programEditorController.getTimelineGridProperty().get() * voiceController.getTotal()) {
+            Platform.runLater(() -> timelineParent.getStyleClass().add("timeline-item-highlighted"));
+        }
     }
 }
