ce9a3af01d0e58fbc15cec601a8f717eb060cbb5
Charney Kaye <charneykaye@gmail.com>
2024-06-17 02:10:57 -0700

    Fabrication Wrapper [skip ci]

---
+++ engine/src/fabricator/FabricationWrapper.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) XJ Music Inc. (https://xjmusic.com) All Rights Reserved.
+
+#include <utility>
+
+#include "xjmusic/fabricator/FabricationWrapper.h"
+#include "xjmusic/util/CsvUtils.h"
+
+using namespace XJ;
+
+
+FabricationWrapper::FabricationWrapper(Fabricator *fabricator) {
+  this->fabricator = fabricator;
+}
+
+
+FabricationException FabricationWrapper::exception(const std::string &message) {
+  return FabricationException(formatLog(message));
+}
+
+
+std::string FabricationWrapper::formatLog(const std::string &message) {
+  return "[segId=" + std::to_string(fabricator->getSegment().id) + "] " + message;
+}
+
+
+void FabricationWrapper::reportMissing(const std::map<std::string, std::string> &traces) {
+  try {
+    fabricator->addWarningMessage("Entity not found! " + CsvUtils::from(traces));
+
+  } catch (const std::exception &e) {
+    spdlog::warn("Failed to create SegmentMessage", e.what());
+  }
+}
+
