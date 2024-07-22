e3170796a02ff67a1d7d448d3dc972989bd47db7
Charney Kaye <charneykaye@gmail.com>
2024-07-22 00:13:29 -0700

    Checkpoint

--- engine/example/XJPlayer.cpp
+++ engine/example/XJPlayer.cpp
@@ -5,7 +5,7 @@
 #include "xjmusic/Engine.h"
 #include "xjmusic/util/CsvUtils.h"
 
-int main(int argc, char *argv[]) {
+int main(const int argc, char *argv[]) {
   // Check if at least one argument was passed
   if (argc <= 1) {
     std::cout << "Must pass the path to an XJ music workstation .xj project as the first argument!" << std::endl;
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
   std::cout << "Will open project: " << pathToProjectFile << std::endl;
 
   try {
-    std::unique_ptr<Engine> engine = std::make_unique<Engine>(
+    const std::unique_ptr<Engine> engine = std::make_unique<Engine>(
         pathToProjectFile,
         Fabricator::ControlMode::Auto,
         std::nullopt,
