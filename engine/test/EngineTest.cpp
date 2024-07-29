bbb153624ebb717376ec59c9cce2f0f8642496d0
Charney Kaye <charneykaye@gmail.com>
2024-07-29 03:36:45 -0700

    TODO why are we missing many audios? NOTE: Each of these individual groups of tests (for one instrument type) are passing, but when all instrument types are present, the test fails. [skip ci]

--- engine/test/EngineTest.cpp
+++ engine/test/EngineTest.cpp
@@ -41,12 +41,12 @@ protected:
 	 */
 	static std::string computeKey(const unsigned long long atChainMicros, const AudioScheduleEvent& event)
 	{
-		return StringUtils::zeroPadded(atChainMicros, 12) + "_" + AudioScheduleEvent::toString(event.type) + "_" + event.audio.getId()
+		return StringUtils::zeroPadded(atChainMicros, 12) + "_" + AudioScheduleEvent::toString(event.type)
 			+ "_" + StringUtils::toProperSlug(event.audio.getAudio()->name)
-			+ "_startAt" + std::to_string(event.audio.getStartAtChainMicros())
-			+ "_stopAt" + std::to_string(event.audio.getStopAtChainMicros())
-			+ "_fromVolume" + std::to_string(static_cast<int>(event.audio.getFromVolume() * 100))
-			+ "_toVolume" + std::to_string(static_cast<int>(event.audio.getToVolume() * 100));
+			+ "_at[" + StringUtils::zeroPadded(event.audio.getStartAtChainMicros(), 12)
+			+ "," + StringUtils::zeroPadded(event.audio.getStopAtChainMicros(), 12) + "]"
+			+ "_vol[" + StringUtils::zeroPadded(static_cast<int>(event.audio.getFromVolume() * 100), 3)
+			+ "," + StringUtils::zeroPadded(static_cast<int>(event.audio.getToVolume() * 100), 3) + "]";
 	}
 
 	void SetUp() override
@@ -106,13 +106,13 @@ TEST_F(XJEngineTest, CorrectlyDubsAudio)
 	// Project, library, template
 	const auto pProject = subject->getProjectContent()->put(ContentFixtures::buildProject());
 	const auto pLibrary = subject->getProjectContent()->put(ContentFixtures::buildLibrary(pProject, "Test Library"));
-	const auto pTemplate = subject->getProjectContent()->put(ContentFixtures::buildTemplate(pProject, "Test Template", "test", "memeTaxonomy=[]"));
+	const auto pTemplate = subject->getProjectContent()->put(ContentFixtures::buildTemplate(pProject, "Test Template", "test", "memeTaxonomy=[]\nintensityAutoCrescendoEnabled=false\n"));
 	subject->getProjectContent()->put(ContentFixtures::buildTemplateBinding(pTemplate, pLibrary));
 	// Macro program
 	const auto pProgramMacro = subject->getProjectContent()->put(
 		ContentFixtures::buildProgram(pLibrary, Program::Type::Macro, Program::State::Published, "Test Macro", "C", 60));
 	const auto pProgramMacroSequence = subject->getProjectContent()->put(
-		ContentFixtures::buildProgramSequence(pProgramMacro, 4, "Test Macro Sequence", 1, "C"));
+		ContentFixtures::buildProgramSequence(pProgramMacro, 8, "Test Macro Sequence", 1, "C"));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceBinding(pProgramMacroSequence, 0));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceBinding(pProgramMacroSequence, 1));
 	// Main program
@@ -120,19 +120,40 @@ TEST_F(XJEngineTest, CorrectlyDubsAudio)
 		ContentFixtures::buildProgram(pLibrary, Program::Type::Main, Program::State::Published, "Test Main", "C", 60));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramMeme(pProgramMain, "Apples"));
 	const auto pProgramMainSequence = subject->getProjectContent()->put(
-		ContentFixtures::buildProgramSequence(pProgramMain, 4, "Test Main Sequence", 1, "C"));
+		ContentFixtures::buildProgramSequence(pProgramMain, 8, "Test Main Sequence", 1, "C"));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceChord(pProgramMainSequence, 0, "C"));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceChord(pProgramMainSequence, 2, "G"));
+	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceChord(pProgramMainSequence, 4, "C"));
+	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceChord(pProgramMainSequence, 6, "G"));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceBinding(pProgramMainSequence, 0));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceBinding(pProgramMainSequence, 1));
 	subject->getProjectContent()->put(ContentFixtures::buildProgramSequenceBinding(pProgramMainSequence, 2));
+	// Beat program
+	const auto pProgramBeat = subject->getProjectContent()->put(
+		ContentFixtures::buildProgram(pLibrary, Program::Type::Beat, Program::State::Published, "Test Beat", "C", 60));
+	const auto pProgramBeatVoice = subject->getProjectContent()->put(
+		ContentFixtures::buildProgramVoice(pProgramBeat, Instrument::Type::Drum, "Kick & Snare"));
+	const auto pProgramBeatTrackKick = subject->getProjectContent()->put(
+		ContentFixtures::buildProgramVoiceTrack(pProgramBeatVoice, "KICK"));
+	const auto pProgramBeatTrackSnare = subject->getProjectContent()->put(
+		ContentFixtures::buildProgramVoiceTrack(pProgramBeatVoice, "SNARE"));
+	const auto pProgramBeatSequence = subject->getProjectContent()->put(
+		ContentFixtures::buildProgramSequence(pProgramBeat, 8, "Test Beat Sequence", 1, "C"));
+	subject->getProjectContent()->put(
+		ContentFixtures::buildProgramSequenceBinding(pProgramBeatSequence, 0));
+	const auto pProgramBeatPattern = subject->getProjectContent()->put(
+		ContentFixtures::buildProgramSequencePattern(pProgramBeatSequence, pProgramBeatVoice, 2, "Groove"));
+	subject->getProjectContent()->put(
+		ContentFixtures::buildProgramSequencePatternEvent(pProgramBeatPattern, pProgramBeatTrackKick, 0, 1, "X", 1));
+	subject->getProjectContent()->put(
+		ContentFixtures::buildProgramSequencePatternEvent(pProgramBeatPattern, pProgramBeatTrackSnare, 1, 1, "X", 1));
 	// Percussion Loop instrument
 	const auto pInstrumentPercussionLoop = subject->getProjectContent()->put(
 		ContentFixtures::buildInstrument(
 			pLibrary, Instrument::Type::Percussion, Instrument::Mode::Loop, Instrument::State::Published, "Perc Loop 1"));
 	subject->getProjectContent()->put(
 		ContentFixtures::buildInstrumentAudio(
-			pInstrumentPercussionLoop, "Perc Loop 1 Audio", "perc-loop-1.wav", 0, 4, 60, 1, "X", "X", 1));
+			pInstrumentPercussionLoop, "Perc Loop 1 Audio", "perc-loop-1.wav", 0, 2, 60, 1, "X", "X", 1));
 	// Pad Chord instrument
 	const auto pInstrumentPadChord = subject->getProjectContent()->put(
 		ContentFixtures::buildInstrument(
@@ -150,6 +171,16 @@ TEST_F(XJEngineTest, CorrectlyDubsAudio)
 	subject->getProjectContent()->put(
 		ContentFixtures::buildInstrumentAudio(
 			pInstrumentHookLoop, "Hook Loop 1 Audio", "hook-loop-1.wav", 0, 4, 60, 1, "X", "X", 1));
+	// Drum Event Instrument
+	const auto pInstrumentDrumEvent = subject->getProjectContent()->put(
+		ContentFixtures::buildInstrument(
+			pLibrary, Instrument::Type::Drum, Instrument::Mode::Event, Instrument::State::Published, "Drum Event 1"));
+	subject->getProjectContent()->put(
+		ContentFixtures::buildInstrumentAudio(
+		pInstrumentDrumEvent, "Kick", "drum-event-kick.wav", 0, 2, 60, 1, "KICK", "X", 1));
+	subject->getProjectContent()->put(
+		ContentFixtures::buildInstrumentAudio(
+		pInstrumentDrumEvent, "Snare", "drum-event-snare.wav", 0, 2, 60, 1, "SNARE", "X", 1));
 
 	subject->start("Test Template");
 	unsigned long long atChainMicros = 0;
@@ -165,8 +196,77 @@ TEST_F(XJEngineTest, CorrectlyDubsAudio)
 	}
 	subject->finish(false);
 
-	// TODO confirm detailed results
+	// TODO why are we missing many audios? NOTE: Each of these individual groups of tests (for one instrument type) are passing, but when all instrument types are present, the test fails.
 
-	// check that all the audio scheduling events were seen
-	ASSERT_TRUE(seenAudioSchedulingEventKeys.size() > 10);
+	// Check that Perc Loop 1 was dubbed correctly
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_PercLoop1Audio_at[000000000000,000002000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_PercLoop1Audio_at[000002000000,000004000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_PercLoop1Audio_at[000004000000,000006000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_PercLoop1Audio_at[000006000000,000008000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_PercLoop1Audio_at[000008000000,000010000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_PercLoop1Audio_at[000010000000,000012000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_PercLoop1Audio_at[000012000000,000014000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_PercLoop1Audio_at[000014000000,000016000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000003000000_Delete_PercLoop1Audio_at[000000000000,000002000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000005000000_Delete_PercLoop1Audio_at[000002000000,000004000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000007000000_Delete_PercLoop1Audio_at[000004000000,000006000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000009000000_Delete_PercLoop1Audio_at[000006000000,000008000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000009000000_Delete_PercLoop1Audio_at[000006000000,000008000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000011000000_Delete_PercLoop1Audio_at[000008000000,000010000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000013000000_Delete_PercLoop1Audio_at[000010000000,000012000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000015000000_Delete_PercLoop1Audio_at[000012000000,000014000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000017000000_Delete_PercLoop1Audio_at[000014000000,000016000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	// Check that Pad Chord 1 was dubbed correctly
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_PadChord1C_at[000000000000,000002000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_PadChord1C_at[000004000000,000006000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_PadChord1C_at[000008000000,000010000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_PadChord1C_at[000012000000,000014000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000002000000_Create_PadChord1C_at[000016000000,000018000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000002000000_Create_PadChord1C_at[000020000000,000022000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000003000000_Delete_PadChord1C_at[000000000000,000002000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000007000000_Delete_PadChord1C_at[000004000000,000006000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000009000000_Create_PadChord1C_at[000024000000,000026000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000009000000_Create_PadChord1C_at[000028000000,000030000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000011000000_Delete_PadChord1C_at[000008000000,000010000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000015000000_Delete_PadChord1C_at[000012000000,000014000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000019000000_Delete_PadChord1C_at[000016000000,000018000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000023000000_Delete_PadChord1C_at[000020000000,000022000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000027000000_Delete_PadChord1C_at[000024000000,000026000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000031000000_Delete_PadChord1C_at[000028000000,000030000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	// Check that Hook Loop 1 was dubbed correctly
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_HookLoop1Audio_at[000000000000,000004000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000000000000_Create_HookLoop1Audio_at[000004000000,000008000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_HookLoop1Audio_at[000008000000,000012000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000001000000_Create_HookLoop1Audio_at[000012000000,000016000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000002000000_Create_HookLoop1Audio_at[000016000000,000020000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000002000000_Create_HookLoop1Audio_at[000020000000,000024000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000005000000_Delete_HookLoop1Audio_at[000000000000,000004000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000009000000_Create_HookLoop1Audio_at[000024000000,000028000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000009000000_Create_HookLoop1Audio_at[000028000000,000032000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000009000000_Delete_HookLoop1Audio_at[000004000000,000008000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000013000000_Delete_HookLoop1Audio_at[000008000000,000012000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000017000000_Delete_HookLoop1Audio_at[000012000000,000016000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000021000000_Delete_HookLoop1Audio_at[000016000000,000020000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000025000000_Delete_HookLoop1Audio_at[000020000000,000024000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000029000000_Delete_HookLoop1Audio_at[000024000000,000028000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+	EXPECT_TRUE(seenAudioSchedulingEventKeys.find("000033000000_Delete_HookLoop1Audio_at[000028000000,000032000000]_vol[100,100]") != seenAudioSchedulingEventKeys.end());
+// Check that the beat program was dubbed correctly with the drum instrument
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Kick_at[000000000000,000001000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Kick_at[000002000000,000003000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Kick_at[000004000000,000005000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Kick_at[000006000000,000007000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Snare_at[000001000000,000002000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Snare_at[000003000000,000004000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Snare_at[000005000000,000006000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000000000000_Create_Snare_at[000007000000,000008000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000002000000_Delete_Kick_at[000000000000,000001000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000003000000_Delete_Snare_at[000001000000,000002000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000004000000_Delete_Kick_at[000002000000,000003000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000005000000_Delete_Snare_at[000003000000,000004000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000006000000_Delete_Kick_at[000004000000,000005000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000007000000_Delete_Snare_at[000005000000,000006000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000008000000_Delete_Kick_at[000006000000,000007000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+EXPECT_TRUE(seenAudioSchedulingEventKeys.find( "000009000000_Delete_Snare_at[000007000000,000008000000]_vol[100,100]")!= seenAudioSchedulingEventKeys.end());
+
+
 }
